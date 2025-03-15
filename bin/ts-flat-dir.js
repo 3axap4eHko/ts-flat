1b2de2efaab88dd1be61ee35319f67ddd384357f
Ivan Zakharchanka <3axap4eHko@gmail.com>
2025-03-15 19:10:18 -0400

    adds support multi-pattern dir mode

--- bin/ts-flat-dir.js
+++ bin/ts-flat-dir.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 import { resolve, basename } from 'node:path';
-import { access, mkdir } from 'node:fs/promises';
+import { access, mkdir, copyFile } from 'node:fs/promises';
 import FastGlob from 'fast-glob';
 import { flat } from '../build/index.js';
 
 const { glob } = FastGlob;
 
-const globPattern = process.argv[2];
-if (typeof globPattern !== 'string') {
-  console.error(`Glob pattern is not provided`);
+const patterns = process.argv.slice(2, -1);
+if (!patterns.every(pattern => typeof pattern === 'string')) {
+  console.error(`Glob patterns are not provided`);
   process.exit(1);
 }
 
 const outputDir = (() => {
   try {
-    return resolve(process.argv[3]);
+    return resolve(process.argv.slice(-1).pop());
   } catch (e) {
     console.error(`Output directory is not provided`);
     process.exit(1);
@@ -23,17 +23,22 @@ const outputDir = (() => {
 
 await mkdir(outputDir, { recursive: true });
 
-const files = await glob(globPattern, { dot: true });
+const files = await Promise.all(patterns.map(pattern => glob(pattern, { absolute: true, dot: true })));
+const uniqueFiles = [...new Set(files.flat())];
 
 await Promise.all(
-  files
+  uniqueFiles
     .map(async (file) => {
       const inputBasename = basename(file);
       const outputFilename = resolve(outputDir, inputBasename);
       if(false !== await access(outputFilename).catch(() => false)) {
         console.error(`Filename ${outputFilename} already exists`);
       }
-      await flat(file, outputFilename);
+      if (outputFilename.endsWith('.d.ts')) {
+        await copyFile(file, outputFilename);
+      } else {
+        await flat(file, outputFilename);
+      }
     })
 ).catch(e => {
   console.error(e);
